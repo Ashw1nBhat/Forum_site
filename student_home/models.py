1ff68c020ed46ae24070399a46ef0536438a4cf5
Ashw1nBhat <ashwinbhat31@gmail.com>
2020-05-08 13:22:50 +0530

    created test database model with question & answer

--- student_home/models.py
+++ student_home/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils import timezone
+from django.utils.timezone import datetime,timedelta
 
-# Create your models here.
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.question_text  
+    
+    
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    answer_text = models.CharField(max_length=200) 
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.answer_text
+
